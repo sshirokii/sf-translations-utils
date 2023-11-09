19735d042874952919c8f20870908a4f81919677
Sergei Shirokii <sergei_shirokii@epam.com>
2023-11-09 12:03:54 +0400

    Refactor of the script
    - Script minor refactoring

--- insertLabels.js
+++ insertLabels.js
@@ -14,29 +14,6 @@ const PATH_CUSTOM_LABELS =
   'force-app/main/default/labels/CustomLabels.labels-meta.xml';
 const XML_TRANSLATION_FILE_SUFFIX = '.translation-meta.xml';
 
-// ATTRIBUTES TO SET
-const NEW_TRANSLATIONS_FILE_PATH = 'input.csv';
-// default category value
-const CATEGORY = 'Service';
-const CUSTOM_LABEL_LOCALE = 'en_GB';
-
-// start processing...
-const data = readFileSync(NEW_TRANSLATIONS_FILE_PATH, 'utf8');
-const [header, ...csvRows] = data.split('\n');
-const [, , ...languages] = header.split(';');
-
-// custom category to set ? add column to destructuring
-// ([name, category, ...labels]) => ({ name, catergory, ...
-const rows = csvRows
-  .map((row) => row.split(';'))
-  .map(([name, ...labels]) => ({
-    name,
-    ...languages.reduce(
-      (acc, curr, index) => ({ ...acc, [curr]: labels[index] }),
-      {},
-    ),
-  }));
-
 const config = {
   customLabel: (language) => ({
     getLabels: ({ customLabels: { labels } }) => labels,
@@ -72,18 +49,57 @@ const config = {
   }),
 };
 
+// Attributes to set
+const NEW_TRANSLATIONS_FILE_PATH = 'input.csv';
+const CATEGORY = 'Service'; // default category value
+const CUSTOM_LABEL_LOCALE = 'en_GB';
+
+// Start processing...
+const data = readFileSync(NEW_TRANSLATIONS_FILE_PATH, 'utf8');
+const [header, ...csvRows] = data.split('\n');
+const [, , ...languages] = header.split(';');
+
+// custom category to set ? add column to destructuring
+// ([name, category, ...labels]) => ({ name, catergory, ...
+const rows = csvRows
+  .map((row) => row.split(';'))
+  .map(([name, ...labels]) => ({
+    name,
+    ...languages.reduce(
+      (acc, curr, index) => ({ ...acc, [curr]: labels[index] }),
+      {},
+    ),
+  }));
+
 languages.forEach((language) => {
   const { filePath, getLabels, createLabel, getXmlObject, keyProperty } =
     getConfig(language);
 
   const newLabels = rows.map(createLabel);
   const existingLabels = existsSync(filePath)
-    ? getLabels(xmlParser.parse(readFileSync(filePath))).map((label) => ({
-        ...label,
-      }))
+    ? getExistingLabels(filePath, getLabels)
     : [];
+  const mergedLabels = getMergedLabels(newLabels, keyProperty, existingLabels);
 
-  const mergedLabels = newLabels
+  const xml = xmlBuilder.build(getXmlObject(mergedLabels));
+  writeFileSync(filePath, xml);
+});
+
+function getConfig(language) {
+  const { customLabel, translation } = config;
+  return language === CUSTOM_LABEL_LOCALE
+    ? customLabel(language)
+    : translation(language);
+}
+
+function getExistingLabels(filePath, getLabels) {
+  return getLabels(xmlParser.parse(readFileSync(filePath))).map((label) => ({
+    ...label,
+  }));
+}
+
+function getMergedLabels(newLabels, keyProperty, existingLabels) {
+  return newLabels
     .reduce(
       (acc, customLabel) => [
         ...acc.filter(
@@ -95,14 +111,4 @@ languages.forEach((language) => {
       existingLabels,
     )
     .sort((a, b) => a[keyProperty].localeCompare(b[keyProperty]));
-
-  const xml = xmlBuilder.build(getXmlObject(mergedLabels));
-  writeFileSync(filePath, xml);
-});
-
-function getConfig(language) {
-  const { customLabel, translation } = config;
-  return language === CUSTOM_LABEL_LOCALE
-    ? customLabel(language)
-    : translation(language);
 }
